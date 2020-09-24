37e7e03f52f1155216830c0a8fc51a9047f0b98c
ruby <jenny56292001@gmail.com>
2020-09-24 16:54:46 +0800

    0924 class

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
 app.get('/',(req, res)=>{
     // res.send('<h1>Hello World</h1>'); //html
     res.render('home',{name: 'Ruby'}); //render for 樣板引擎
@@ -18,6 +22,24 @@ app.get('/json-sales2',(req,res)=>{
     const sales = require(__dirname + '/../data/sales');
     res.render('abc/def/json-sales2',{sales});
     // res.json(sales);
+});
+
+app.get('/try-query',(req,res)=>{
+    res.json(req.query);
+});
+
+
+app.post('/try-post',(req,res)=>{
+    res.json(req.body);
+});
+
+app.get('/try-post-form',(req,res)=>{
+    // res.render('try-post-form',{email:'', password:''});
+    res.render('try-post-form');
+});
+
+app.post('/try-post-form',(req,res)=>{
+    res.render('try-post-form',req.body);
 });
 
 app.set('view engine','ejs');
