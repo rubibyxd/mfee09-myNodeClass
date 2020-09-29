57c18d16dd8c269f481e22fbde51a8cf215bd296
ruby <jenny56292001@gmail.com>
2020-09-29 11:42:51 +0800

    node class morning 0929

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -1,14 +1,16 @@
 const db = require(__dirname + '/../db_connect2');
 const express = require('express');
+const moment = require('moment-timezone');
 const router = express.Router();
 
 async function getListData (req){
     const output = {
         page: 1,
-        perPage: 5,
+        perPage: 10,
         totalRows: 0,
         totalPages: 0,
-        rows: []
+        rows: [],
+        pages: []
     };
 
     const [ [ {totalRows} ] ] = await db.query("SELECT COUNT(1) totalRows FROM address_book");
@@ -25,9 +27,37 @@ async function getListData (req){
         }else{
             output.page = page;
         }
+
+        if(output.totalPages < 5){
+            for(let i=1; i<=output.totalPages; i++){
+                output.pages.push(i);
+            }
+        }else{
+            const fAr = [], bAr = [];
+
+            //從前面開始數
+            for(let i = output.page -3; i<=output.totalPages; i++){
+                if(i >= 1){
+                    fAr.push(i);
+                }
+                if(fAr.length >= 5) break;
+            }
+            //從後面開始數
+            for(let i = output.page + 3; i >= 1; i--){
+                if(i <= output.totalPages){
+                    bAr.unshift(i);
+                }
+                if(bAr.length >= 7) break;
+            }
+            output.pages = fAr.length > bAr.length ? fAr : bAr;
+        }
+
         let sql = `SELECT * FROM address_book ORDER BY sid DESC LIMIT ${(output.page - 1) * output.perPage},${output.perPage}`;
 
         const [r2] = await db.query(sql);
+        r2.forEach(element => {
+            element.birthday = moment(element.birthday).format('YYYY-MM-DD');
+        });
         output.rows = r2;
     }
     return output;
